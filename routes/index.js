0f84e18b5995bd70662da713dbee9c6f4ca4e65b
Michael Spitz <michael.spitz@codeimmersives.com>
2023-03-20 11:30:58 -0400

    Initial working Backend

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const indexRouter = express.Router();
+
+const Blog = require ('../models/Blogs');
+
+/* GET home page. */
+indexRouter.get('/', async function(req, res, next) {
+  res.render('index', { title: 'Express' });
+// try {
+//   const allBlogs = await Blog.find({});
+//   res.json({blogs: allBlogs});
+// } catch (error) {
+//   console.log(error);
+// }
+});
+
+module.exports = indexRouter;
